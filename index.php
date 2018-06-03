2f0a027811c526b4867aeeb325185735e139525f
markohoza <39862482+markohoza@users.noreply.github.com>
2018-06-03 12:02:10 +0200

    test

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta name="author" content="Marko Hoza">
+	<meta name="author" content="Marko Hozaa">
 	<title>webdesign2018</title>
     <link rel="icon" type="image/png" href="img/favicon.png">
     <link rel='stylesheet' href='node_modules/swiper/dist/css/swiper.min.css' type='text/css'/>
